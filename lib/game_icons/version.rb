35e1295678790bd0e36721fb35f2c2a96b065eea
Andy Meneely <andy@se.rit.edu>
2015-06-22 08:47:51 -0400

    I dub thee v0.8.0

--- lib/game_icons/version.rb
+++ lib/game_icons/version.rb
@@ -3,5 +3,5 @@ module GameIcons
   # e.g. "0.1.0.20150413.alpha" will eventually become "0.1.0.20150413"
   # Uses semantic versioning: http://semver.org/
   # The date after the semver numbers is the build date take from game-icons.net
-  VERSION = "0.7.0.20150615"
+  VERSION = "0.8.0.20150622"
 end
