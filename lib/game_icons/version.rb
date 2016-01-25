0ccc93fff79f9efdc0c1cf361474b0b38979650a
Andy Meneely <andy@se.rit.edu>
2016-01-25 09:24:03 -0500

    Fix version number

--- lib/game_icons/version.rb
+++ lib/game_icons/version.rb
@@ -3,5 +3,5 @@ module GameIcons
   # e.g. "0.1.0.20150413.alpha" will eventually become "0.1.0.20150413"
   # Uses semantic versioning: http://semver.org/
   # The date after the semver numbers is the build date from game-icons.net
-  VERSION = '0.26.0'
+  VERSION = '0.26.1.20160124'
 end
